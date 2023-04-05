a2f29f370c37f1bb826b0fce816b27e57125896a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-04-04 22:33:14 -0700

    CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middlewares
+
+// Implementing CORS
+app.use(cors());
+app.options('*', cors()); // to allow PATCH & DELETE reqs
 // Serving static files
 // app.use(express.static('./public'));
 app.use(express.static(path.join(__dirname, 'public')));
